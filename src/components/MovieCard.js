e2e1b48db961c77900ebceb4fa41bb35d263ee42
prince475 <danielmwai475@gmail.com>
2023-01-13 07:56:53 +0300

    Looking at distructing props

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,39 @@
 import React from "react";
 
-function MovieCard({
-  title,
-  posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
-  genres,
-}) {
+// function MovieCard({
+//   title,
+//   posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
+//   genres,
+// }) {
+//   return (
+//     <div className="movie-card">
+//       <img src={posterSrc} alt={title} />
+//       <h2>{title}</h2>
+//       <small>{genres.join(", ")}</small>
+//     </div>
+//   );
+// }
+
+// export default MovieCard;
+
+
+//without Destructing
+// function MovieCard(props) {
+//   return (
+//     <div className="movie-card">
+//       <img src={props.posterSrc} alt={props.title} />
+//       <h2>{props.title}</h2>
+//       <small>{props.genres.join(", ")}</small>
+//     </div>
+//   );
+// }
+
+
+//with Destructing
+
+function MovieCard({ title, 
+                    posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
+                    genres }) {
   return (
     <div className="movie-card">
       <img src={posterSrc} alt={title} />
